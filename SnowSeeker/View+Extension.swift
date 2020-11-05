51267a63d2c7c27a67be68bc945673ab145b1ee7
SwiftViktor <panafviktor@gmail.com>
2020-11-05 18:26:33 +0300

    add WelcomeView

---
+++ SnowSeeker/View+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  View+Extension.swift
+//  SnowSeeker
+//
+//  Created by Viktor on 05.11.2020.
+//
+
+import SwiftUI
+
+extension View {
+    func phoneOnlyStackNavigationView() -> some View {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return AnyView(self.navigationViewStyle(StackNavigationViewStyle()))
+        } else {
+            return AnyView(self)
+        }
+    }
+}
---
+++ SnowSeeker/WelcomeView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeView.swift
+//  SnowSeeker
+//
+//  Created by Viktor on 05.11.2020.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome to SnowSeeker!")
+                .font(.largeTitle)
+
+            Text("Please select a resort from the left-hand menu; swipe from the left edge to show it.")
+                .foregroundColor(.secondary)
+        }
+    }
+}
